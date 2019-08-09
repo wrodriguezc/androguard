15b1199de3e2444d0a2a668f3abd2fa2970d4e66
William Rodriguez <william@vivogaming.com>
2019-08-09 10:57:41 -0600

    Compresed source code after analysis is finished

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ RUN pip install -U androguard
 RUN pip install graphviz
 
 #Setup SSH server
-RUN apt-get update && apt-get install -y openssh-server graphviz
+RUN apt-get update && apt-get install -y openssh-server graphviz zip
 RUN mkdir /var/run/sshd
 RUN sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 RUN sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
--- bin/decompile.sh
+++ bin/decompile.sh
@@ -4,7 +4,7 @@
 rm -rf /data/static_analysis/$1
 
 #Create required directories
-mkdir /data/static_analysis/$1 && mkdir /data/static_analysis/$1/resources && mkdir /data/static_analysis/$1/manifest && mkdir /data/static_analysis/$1/decompiled
+mkdir -p /data/static_analysis && mkdir /data/static_analysis/$1 && mkdir /data/static_analysis/$1/resources && mkdir /data/static_analysis/$1/manifest && mkdir /data/static_analysis/$1/decompiled
 
 #Extract manifest data
 androguard axml -o /data/static_analysis/$1/manifest/manifest.xml -i /data/samples/$1/$2
@@ -15,3 +15,6 @@ androguard arsc -o /data/static_analysis/$1/resources/resources.txt -i /data/sam
 #Extract the package name
 package=$(cat /data/static_analysis/$1/manifest/manifest.xml | grep -Po '.*package="\K.*?(?=".*)' | tr '.' '/')
 androguard decompile -o /data/static_analysis/$1/decompiled -f png -i /data/samples/$1/$2 --limit "^L$package/.*"
+
+#Compress the source code of the app for distribution
+cd /data/static_analysis/$1 && zip -r decompiled.zip decompiled
